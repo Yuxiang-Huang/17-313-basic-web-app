9496b0f239d70c0579e9f49d20ffd0eebdcafa58
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-10-06 10:17:47 -0400

    feat: implement support for the query 'What is your Andrew ID?'

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,15 +1,19 @@
 export default function QueryProcessor(query: string): string {
-  if (query.toLowerCase().includes("shakespeare")) {
-    return (
-      "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
-      "English poet, playwright, and actor, widely regarded as the greatest " +
-      "writer in the English language and the world's pre-eminent dramatist."
-    );
-  }
+	if (query.toLowerCase().includes("andrew id")) {
+		return "yh4";
+	}
 
-  if (query.toLowerCase().includes("name")) {
-    return "Rohan";
-  }
+	if (query.toLowerCase().includes("shakespeare")) {
+		return (
+			"William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+			"English poet, playwright, and actor, widely regarded as the greatest " +
+			"writer in the English language and the world's pre-eminent dramatist."
+		);
+	}
 
-  return "";
+	if (query.toLowerCase().includes("name")) {
+		return "Rohan";
+	}
+
+	return "";
 }
