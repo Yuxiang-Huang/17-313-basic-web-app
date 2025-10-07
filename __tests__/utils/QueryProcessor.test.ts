0de0693aa4a27fea6aea39efb8700fb163e43b38
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-10-07 10:09:43 -0400

    tests: fix test case for name

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -21,7 +21,7 @@ describe("QueryProcessor", () => {
 	test("should return name", () => {
 		const query = "What is your name?";
 		const response: string = QueryProcessor(query);
-		expect(response).toBe("Rohan");
+		expect(response).toBe("yh4");
 	});
 
 	test("should return Andrew ID", () => {
